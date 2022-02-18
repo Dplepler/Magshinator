0ac7a27cca9800ecc18cbe56770f159ba2eeb44f
Bachuchee <gal.aloni@outlook.com>
2022-02-18 18:50:38 +0200

    Add livedata for question text

--- fuckinator-android/app/src/main/java/com/example/fuckinator/choicescreen/ChoiceViewModel.kt
+++ fuckinator-android/app/src/main/java/com/example/fuckinator/choicescreen/ChoiceViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.fuckinator.choicescreen
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ChoiceViewModel: ViewModel() {
 
     private var questionData = arrayOf<String> ("האם הדמות שך סחית?", "האם הדמות שלך גרה מחוץ לבאר שבע?", "שיער שופע?", "גיי?", "חתיך?", "מצחיק?", "אפס?", "חבר של ליחי סוויסו?")
+
+    private var curQuestion = MutableLiveData<String>()
 
     private var characterList = mutableListOf<Character>()
 
